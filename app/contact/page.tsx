80709d4e419b315618c449b77ff3d60ea29c77ed
CodeVista Solutions <solutionscodevista@gmail.com>
2026-07-29 20:28:28 +0530

    aaa

---
+++ app/contact/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/app/components/Navbar";
+import Email from "@/app/components/Email";
+import Footer from "@/app/components/Footer";
+
+export default function EmailPage() {
+  return (
+    <>
+      <Navbar />
+      <Email />
+      <Footer />
+    </>
+  );
+}
