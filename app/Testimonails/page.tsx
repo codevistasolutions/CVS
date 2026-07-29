297a39837f554f7e2b7a49bd19aed18630d900d3
CodeVista Solutions <solutionscodevista@gmail.com>
2026-07-29 20:03:58 +0530

    xyz

---
+++ app/Testimonails/page.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/app/components/Navbar";
+import Testimonials from "@/app/components/Testimonials";
+import Footer from "@/app/components/Footer";
+
+export default function TestimonialsPage() {
+  return (
+    <>
+      <Navbar />
+      <Services />
+      <Footer />
+    </>
+  );
+}
