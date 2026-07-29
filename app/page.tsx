c898a2b23c0f5aa37307ccd2aaec82900e88cf4b
CodeVista Solutions <solutionscodevista@gmail.com>
2026-07-29 20:43:18 +0530

    single page to multiple

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,9 @@ import LogosSection from './components/LogosSection'
 import Features from './components/Features'
 import Working from './components/Working'
 import About from './components/About'
-import Testimonials from './components/Testimonials'
 import Email from './components/Email'
 import Team from './components/Team'
-import Services from './components/Services'
 import ProjectDisplay from './components/ProjectDisplay'
-import Steps from './components/Steps'
 
 
 export default function Home() {
@@ -24,11 +21,8 @@ export default function Home() {
       <Features />
       <Working />
       {/* <About /> */}
-      <Services />
       {/* <Team /> */}
-      <Steps />
       {/* <ProjectDisplay /> */}
-      <Testimonials />
       <Email />
 
     
