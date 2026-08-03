05c98507da784fe40572fff0cd3aa7f1eba8794b
CodeVista Solutions <solutionscodevista@gmail.com>
2026-08-03 14:01:57 +0530

    hsbh

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -1,5 +1,5 @@
 import Navbar from "@/app/components/Navbar";
-import PrivacyPolicy from "@/app/components/OurServices/PrivacyPolicy";
+import PrivacyPolicy from "@/app/components/PrivacyPolicy";
 import Footer from "@/app/components/Footer";
 
 export default function Page() {
